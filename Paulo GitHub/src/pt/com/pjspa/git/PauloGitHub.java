4fe8dd9a50d84bdac1bd53e0c45ded89bff1efd5
Paulo Palma <91433008+pjspa@users.noreply.github.com>
2021-10-02 22:08:18 +0100

    Adicionado aquivos upload

---
+++ Paulo GitHub/src/pt/com/pjspa/git/PauloGitHub.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package pt.com.pjspa.git;
+
+/**
+ * @author Paulo.Palma
+ * 
+ * Exemplo Eclipse com GitHub
+ *
+ */
+public class PauloGitHub {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Ola Teste GitHub");
+		// TODO Auto-generated method stub
+
+	}
+
+}
